ed4ecf2e8080146361c99718fb5f36ca75b556c9
Kellyn Arnold <kellynarnold@MacBook-Pro.lan>
2020-07-23 10:47:39 +0100

    Minor edits to data importing/formatting code

--- Code/Import and format data.R
+++ Code/Import and format data.R
@@ -53,11 +53,11 @@ rm(govuk_data_cases, govuk_data_deaths_hosp_dor, nhseng_data_deaths_hosp_dod, on
 # View data
 # cases; deaths_hosp_dor; deaths_hosp_dod; deaths_all_dod
 
-# Factorise data
-cases <- cases %>% mutate_at(vars(Area_name, Area_code, Area_type), funs(as.factor))
-deaths_hosp_dor <- deaths_hosp_dor %>% mutate_at(vars(Area_name, Area_code, Area_type), funs(as.factor))
-deaths_hosp_dod <- deaths_hosp_dod %>% mutate_at(vars(Area_name), funs(as.factor))
-deaths_all_dod <- deaths_all_dod %>% mutate_at(vars(Area_name), funs(as.factor))
+# Factorise data and group by Area_name
+cases <- cases %>% mutate_at(vars(Area_name, Area_code, Area_type), funs(as.factor)) %>% group_by(Area_name)
+deaths_hosp_dor <- deaths_hosp_dor %>% mutate_at(vars(Area_name, Area_code, Area_type), funs(as.factor)) %>% group_by(Area_name)
+deaths_hosp_dod <- deaths_hosp_dod %>% mutate_at(vars(Area_name), funs(as.factor)) %>% group_by(Area_name)
+deaths_all_dod <- deaths_all_dod %>% mutate_at(vars(Area_name), funs(as.factor)) %>% group_by(Area_name)
 
 # Reorder data by date
 cases <- cases %>% arrange(Date)
@@ -65,36 +65,32 @@ deaths_hosp_dor <- deaths_hosp_dor %>% arrange(Date)
 deaths_hosp_dod <- deaths_hosp_dod %>% arrange(Date)
 deaths_all_dod <- deaths_all_dod %>% arrange(Date)
 
-# Define important dates
-date_0 <- min(cases$Date)  # date of first confimed case
-date_max <- max(cases$Date) - 5  # date for which data can be reasonably assumed complete
-date_T <- as.Date("2020-06-01")  # end date for simulation
-date_sd <- as.Date("2020-03-17")  # first full day of social distancing
-date_lockdown <- as.Date("2020-03-24")  # first full day of lockdown
+# Remove entries for which daily or cumulative cases are NA
+cases <- cases %>% filter(!is.na(Daily_cases) | !is.na(Cumulative_cases))
+#deaths_hosp_dor %>% filter(is.na(Daily_deaths) | is.na(Cumulative_deaths))
 
-# Omit data from previous 5 days (due to reporting delays)
+# Define date for which data can be reasonably assumed complete (due to reporting delays)
+date_max <- max(cases$Date) - 5
+
+# Omit data that is incomplete due to reporting delays
 cases <- cases %>% filter(Date <= date_max)
 deaths_hosp_dor <- deaths_hosp_dor %>% filter(Date <= date_max)
 deaths_hosp_dod <- deaths_hosp_dod %>% filter(Date <= date_max)
 deaths_all_dod <- deaths_all_dod %>% filter(Date <= date_max)
 
 # Retain only country-level data for England and remove other data files
-cases_eng <- filter(cases, Area_name == "England")
-deaths_hosp_dor_eng <- filter(deaths_hosp_dor, Area_name == "England")
-deaths_hosp_dod_eng <- filter(deaths_hosp_dod, Area_name == "England")
-deaths_all_dod_eng <- filter(deaths_all_dod, Area_name == "England")
+cases_eng <- cases %>% filter(Area_name == "England")
+deaths_hosp_dor_eng <- deaths_hosp_dor %>% filter(Area_name == "England")
+deaths_hosp_dod_eng <- deaths_hosp_dod %>% filter(Area_name == "England")
+deaths_all_dod_eng <- deaths_all_dod %>% filter(Area_name == "England")
 rm(cases, deaths_hosp_dor, deaths_hosp_dod, deaths_all_dod)
 
 # Calculate cumulative cases at beginning of each day
 # (current counts represent cases at end of each day)
 names(cases_eng)[names(cases_eng) == "Cumulative_cases"] <- 
   "Cumulative_cases_end"
-cases_eng$Cumulative_cases_beg <- 
-  c(0, cases_eng$Cumulative_cases_end[1:(length(cases_eng$Cumulative_cases_end) - 1)])
-cases_eng <- cases_eng %>% 
-  select(Area_name, Area_code, Area_type, Date, 
-         Cumulative_cases_beg, Daily_cases, 
-         Cumulative_cases_end)
+cases_eng <- cases_eng %>% mutate(Cumulative_cases_beg = lag(Cumulative_cases_end, n = 1, default = 0)) %>%
+  relocate(c(Cumulative_cases_beg, Daily_cases), .before = Cumulative_cases_end)
 
 # Calculate growth factor for each time point
 for (t in 2:nrow(cases_eng)) {
@@ -117,14 +113,18 @@ deaths_hosp_dod_eng <- deaths_hosp_dod_eng %>% mutate(Daily_deaths_MA7 = round(r
 deaths_all_dod_eng <- deaths_all_dod_eng %>% mutate(Daily_deaths_MA7 = round(runmean(Daily_deaths, k = 7, alg = "C", endrule = "mean"), 3),
                                                     Cumulative_deaths_MA7 = round(runmean(Cumulative_deaths, k = 7, alg = "C", endrule = "mean"), 3))
 
-# Calculate first full date at which cumulative cases in England exceeded 100
-date_100 <- filter(cases_eng, Cumulative_cases_beg >= 100)$Date[1]
+# Define important dates
+date_0 <- min(cases_eng$Date)  # date of first confimed case in England
+date_T <- as.Date("2020-06-01")  # end date for simulation
+date_sd <- as.Date("2020-03-17")  # first full day of social distancing
+date_lockdown <- as.Date("2020-03-24")  # first full day of lockdown
+date_100 <- min(filter(cases_eng, Cumulative_cases_beg >= 100)$Date)  # first full date at which cumulative cases in England exceeded 100
 
 # Create variable for number of days since date_100
 cases_eng <- cases_eng %>% mutate(Days_since_date_100 = as.numeric(Date - date_100)) %>%
   relocate(Days_since_date_100, .after = Date)
 
-# Create copy of dataset where cumulative cases > 100
+# Create copy of dataset where cumulative cases >= 100 and date <= end date for simulation
 cases_eng_100 <- filter(cases_eng, Date >= date_100 & Date <= date_T)
 
 # ------------------------------------------------------------------------------
